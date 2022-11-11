c9a345a5c8a88e31d72aa088c5c2054c2f42c839
ghostm68 <42044862+ghostm68@users.noreply.github.com>
2022-11-11 07:21:05 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,3 +12,27 @@ return cache.addAll(resourcesToCache);
 })
 );
 });
+// Cache and return requests
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(response=>{
+return response || fetch(e.request);
+})
+);
+});
+
+// Update a service worker
+const cacheWhitelist = ['https://authorwave.gq/'];
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(cacheNames => {
+return Promise.all(
+cacheNames.map(cacheName => {
+if (cacheWhitelist.indexOf(cacheName) === -1) {
+return caches.delete(cacheName);
+}
+})
+);
+})
+);
+});
